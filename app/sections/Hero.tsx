7b43d59fc07ced9fec074ce8304a5183a33c665e
yshak <Vyshakn29@gmail.com>
2025-01-06 20:19:23 +0530

    mission complete

--- app/sections/Hero.tsx
+++ app/sections/Hero.tsx
@@ -5,19 +5,19 @@ import { clipPath } from 'motion/react-client'
 
 function Hero() {
     return (
-        <section className='w-full flexCenter bg-neutral-900'>
+        <section className='w-full flexCenter '>
             <BgText key={1} align={'left-10'} text=' Developer turned Designer Developer turned Designer ' from={'`0'} to='-100vh' />
             <BgText key={2} align={'right-10'} text='designer turned Developer designer turned Developer designer turned Developer ' from={'0'} to='100vh' />
             <div className='flex w-full p-2'>
                 <div className='w-full z-10 h-screen py-4 pl-4'>
-                    <div style={{ clipPath: "polygon(0 0, 100% 0, 100% 80%, 80% 100%, 0 100%)" }} className='w-full rounded-md h-full bg-blue-500'>
+                    <div style={{ clipPath: "polygon(0 0, 100% 0, 100% calc(100% - 4rem), calc(100% - 4rem) 100%, 0 100%)" }} className='w-full rounded-md h-full bg-blue-500'>
                         hey
                     </div>
                 </div>
                 <div className='w-full z-10 h-screen py-4'>
                     <div style={{
-                        clipPath: "inset(0px 0px 20% 0px)"
-                    }} className='w-full rounded-md h-full bg-blue-500'>
+                        clipPath: "inset(0px 0px 10% 0px round 2%)"
+                    }} className='w-full -ml-6 rounded-md h-full bg-blue-500'>
                         hello
                     </div>
                 </div>
